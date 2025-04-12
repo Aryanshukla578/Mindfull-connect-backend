d7d969a4f396dbd6893856d16c4541fb26a331a2
Aryan Shukla <as3061693@gmail.com>
2025-04-13 00:20:19 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["MindConnect"]  # <- Use the existing DB from Compass
